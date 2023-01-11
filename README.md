c89fc1aa76e6f7f6a0f89e3c83d8addc793e3847
Yana Peshko <yanapeshko917@gmail.com>
2023-01-10 22:52:28 -0700

    +html, js, styles - done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # thank_you_lonnie
+
+Small project to say thank you to my BEST friend, for helping me so much.
---
+++ js/script.js
@@ -0,0 +1,59 @@
+"use strict";
+
+window.addEventListener("load", windowLoad);
+
+function windowLoad() {
+  document.documentElement.classList.add("loaded");
+  createPosition();
+
+  window.addEventListener("scroll", createPosition);
+
+  function createPosition() {
+    const contentElement = document.querySelector(".content__container");
+    const windowHeight = window.innerHeight;
+
+    const finalPos = (scrollY / (contentElement.offsetTop - windowHeight)) * 60;
+
+    finalPos < 100 ? forestAnimation(finalPos) : forestAnimation(100);
+  }
+  function forestAnimation(finalPos) {
+    const mountains = document.querySelector(".main-screen__mountains");
+    const trees = document.querySelectorAll(".main-screen__trees");
+    const birds = document.querySelectorAll(".main-screen__birds");
+
+    const mountainsTranslate = (170 / 100) * finalPos;
+    const mountainsScale = 1 + (2 / 100) * finalPos;
+
+    mountains.style.cssText = `
+      transform:
+      translate3d(0, ${mountainsTranslate}%,0)
+      scale(${mountainsScale})
+      `;
+
+    trees.forEach((tree, index) => {
+      const treeTranslate = ((20 * (trees.length - index)) / 100) * finalPos;
+      const treeScale = 1 + (1.5 / 100) * finalPos;
+
+      tree.style.cssText = `
+      transform:
+      translate3d(0, ${treeTranslate}%,0)
+      scale(${treeScale})
+      `;
+    });
+
+    const birdsTranslate = (19 / 100) * finalPos;
+    const birdsScale = 1 + (2 / 100) * finalPos;
+
+    birds[0].style.cssText = `
+      transform:
+      translate3d( -${birdsTranslate}%,0,0)
+      scale(${birdsScale})
+      `;
+
+    birds[1].style.cssText = `
+      transform:
+      translate3d( ${birdsTranslate}%,0,0)
+      scale(${birdsScale})
+      `;
+  }
+}
